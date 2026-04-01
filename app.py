a2c8b70cb9c2b824d36d5a48d32cbd651729a741
anuradha  dwivedi <anuradha.dwivedi2021@gmail.com>
2026-04-01 11:53:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -375,8 +375,8 @@ Respond ONLY in this exact JSON format (no markdown, no extra text):
             'prevention':  '<p>• Dobara try karo</p>'
         })
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=7860)
+    if __name__ == "__main__":
+       app.run(host="0.0.0.0", port=7860)
 
     if __name__ == "__main__":
         app.run(host="0.0.0.0", port=7860)
